cb86044ce756aafb237f014a56f426cca3867968
Jin_00 <anjdal64@gmail.com>
2025-05-04 01:42:26 +0900

    initialize
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

---
+++ src/main/java/com/example/buyornot/BuyOrNotApplication.java
@@ -0,0 +1,13 @@
+package com.example.buyornot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyOrNotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyOrNotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buyornot/BuyOrNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buyornot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyOrNotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
